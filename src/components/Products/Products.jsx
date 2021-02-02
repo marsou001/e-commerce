09c9f0e82c7cfe9683e091bc93cb2d93e8dae392
Marouane Souda <marsou431@gmail.com>
2021-02-02 16:19:56 +0100

    fix UI layout

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -11,7 +11,7 @@ function Products({ products, onAddToCart }) {
             <div className={classes.toolbar} />
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
-                    <Grid key={product.id} item xd={12} xm={6} md={4} lg={3}>
+                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
                         <Product product={product}  onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
