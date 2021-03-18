123b2de625880d172df518a42579dda487dd4eaf
Marouane Souda <marsou431@gmail.com>
2021-03-18 09:31:58 +0100

    refactor: remove comments from Review.jsx

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from 'react';
-// import {commerce} from '../../lib/commerce'
+import React from 'react';
 import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
 
-let data = {};
-
 function Review({ checkoutToken, shippingInfo }) {
-    // const [render, setRender] = useState(false);    
     const paddingStyle = {
         padding: '10px 0'
     };    
     console.log(checkoutToken.live)
-
-    // const getShippingOption = async () => {
-    //     try {
-    //         data = await commerce.checkout.checkShippingOption(checkoutToken.id, {
-    //             shipping_option_id: shippingData.shippingOption,
-    //             country: shippingData.shippingCountry,
-    //             region: shippingData.shippingSubdivision
-    //         });            
-    //         setRender(prevState => !prevState);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     getShippingOption();
-    // }, []);
-
-    // if (!Object.keys(shippingInfo).length) return null;
 
     return (
         <>
